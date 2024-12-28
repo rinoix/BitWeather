1e9875dbeb24afc7727b912b867c80dd6140ab58
rinoix <r202200815kc@jindai.jp>
2024-12-28 17:22:14 +0900

    refactor: add exception

--- app/controllers/utils.py
+++ app/controllers/utils.py
@@ -2,6 +2,9 @@ import requests
 from flask import current_app
 
 def fetch_ip_data(ip):
-    responce = requests.get(f"{current_app.config["IP_API_URL"]}{ip}")
-    ip_data = responce.json()
-    return ip_data
+    try:
+        responce = requests.get(f"{current_app.config["IP_API_URL"]}{ip}")
+        ip_data = responce.json()
+        return ip_data
+    except requests.RequestException as e:
+        return None
