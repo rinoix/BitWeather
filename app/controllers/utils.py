ac62d9e567366665cacdb12b56d80d23d44bda06
rinoix <r202200815kc@jindai.jp>
2024-12-31 11:26:19 +0900

    test: bugfix

--- app/controllers/utils.py
+++ app/controllers/utils.py
@@ -2,7 +2,6 @@ import requests
 from flask import current_app, request
 
 def fetch_ip_data(ip):
-    ip = request.remote_addr
     try:
         response = requests.get(f"{current_app.config['IP_API_URL']}{ip}")
         ip_data = response.json()
