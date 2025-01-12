dd791bf8f3d534bd332849bc8739fdaf75f8c167
rinoix <r202200815kc@jindai.jp>
2025-01-12 17:10:48 +0900

    test: change x_for

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,7 @@ import os
 def create_app():
     app = Flask(__name__)
     
-    app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1)
+    app.wsgi_app = ProxyFix(app.wsgi_app, x_for=3, x_proto=1, x_host=1)
     
     load_dotenv()
     
