a429f462334eb06757fe8b5a7a31c1e0c3c94b6b
rinoix <r202200815kc@jindai.jp>
2025-01-12 17:28:33 +0900

    test: change x_for

--- app/__init__.py
+++ app/__init__.py
@@ -6,12 +6,13 @@ import os
 def create_app():
     app = Flask(__name__)
     
-    app.wsgi_app = ProxyFix(app.wsgi_app, x_for=3, x_proto=1, x_host=1)
+    app.wsgi_app = ProxyFix(app.wsgi_app, x_for=2)
     
     load_dotenv()
     
     app.config["WEATHER_API_KEY"] = os.getenv("WEATHER_API_KEY")
     app.config["IP_API_URL"] = os.getenv("IP_API_URL")
+    app.config["TIME_API_URL"] = os.getenv("TIME_API_URL")
     
     from .routes import main
     app.register_blueprint(main)
